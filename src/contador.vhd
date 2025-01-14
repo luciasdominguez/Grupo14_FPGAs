4b7b99fca064eebbfeb8c112202ce37050df5e34
lunasbaciero <100705764+lunasbaciero@users.noreply.github.com>
2025-01-14 12:49:28 +0100

    Entidades del modo manual y entidad del contador
    En el modo manual se selecciona a través del gestor de entradas un color u otro y se modifica su valor, el cual se va registrando en el contador para poder dar la salida al gestor de salidas.

---
+++ src/contador.vhd
@@ -0,0 +1,40 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.all;
+
+entity color_counter is
+    Port (
+        clk         : in std_logic;
+        reset       : in std_logic;
+        increment   : in std_logic;                        -- Botón para incrementar
+        decrement   : in std_logic;                        -- Botón para decrementar
+        enable      : in std_logic;                        -- Señal de habilitación del contador
+      color_value : out std_logic_vector(5 downto 0)    -- Valor del color
+    );
+end color_counter;
+
+architecture Behavioral of color_counter is
+signal counter : integer range 0 to 64 := 0;
+begin
+
+    -- Proceso del contador
+   process(clk, reset)
+    begin
+        if reset = '1' then
+            counter <= 0;
+        elsif rising_edge(clk) then
+            if enable = '1' then
+                if increment = '1' and counter < 64 then
+                    counter <= counter + 1;
+                elsif decrement = '1' and counter > 0 then
+                    counter <= counter - 1;
+                end if;
+            end if;
+        end if;
+    end process;
+
+    -- Asignación de la salida del contador
+    color_value <= std_logic_vector(to_unsigned(counter, 6));
+
+end Behavioral;
---
+++ src/modo_manual.vhd
@@ -0,0 +1,113 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 14.01.2025 11:26:05
+-- Design Name: 
+-- Module Name: modo_manual - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity rgb_intensity_manager is
+    Port (
+      clk              : in std_logic;                 
+      reset            : in std_logic;               
+      manual_mode_enable : in std_logic;              
+      buttons          : in std_logic_vector(4 downto 0);
+      RGB_out            : out std_logic_vector(17 downto 0)
+
+    );
+end rgb_intensity_manager;
+
+architecture Behavioral of rgb_intensity_manager is
+
+    -- Señales para valores actuales y contadores
+signal R_value, G_value, B_value : std_logic_vector(5 downto 0) := (others => '0');
+    signal active_var, R_var, G_var, B_var : std_logic;
+
+begin
+
+    -- Proceso principal de selección de color
+    --RGB_combined <= R_value & G_value & B_value;
+    process(clk, reset)
+    begin
+        if reset = '1' then
+            active_var <= '0';
+        elsif rising_edge(clk) then
+            if manual_mode_enable = '1' then
+                -- Selección de color activo con botones [0], [1], [2]
+                if buttons(0) = '1' then
+                    active_var <= '1';
+                    R_var <= '1'; 
+                    G_var <= '0'; 
+                    B_var <= '0'; 
+                elsif buttons(1) = '1' then
+                    active_var <= '1';
+                    R_var <= '0'; 
+                    G_var <= '1'; 
+                    B_var <= '0'; 
+                elsif buttons(2) = '1' then
+                    active_var <= '1';
+                    R_var <= '0'; 
+                    G_var <= '0'; 
+                    B_var <= '1'; 
+                end if;
+            end if;
+        end if;
+    end process;
+    -- Asignacion de los valores de cada color tras cada ciclo de reloj
+process(clk)
+    begin
+        if rising_edge(clk) then
+            RGB_out <= R_value & G_value & B_value;
+        end if;
+    end process;
+
+    -- Instanciación de entidades de contadores para R, G, B
+    color_counter_r : entity work.color_counter
+        port map (
+            clk => clk,
+            reset => reset,
+            increment => buttons(3),
+            decrement => buttons(4),
+            enable => R_var,
+            color_value => R_value
+        );
+
+    color_counter_g : entity work.color_counter
+        port map (
+            clk => clk,
+            reset => reset,
+            increment => buttons(3),
+            decrement => buttons(4),
+            enable => G_var,
+            color_value => G_value
+        );
+
+    color_counter_b : entity work.color_counter
+        port map (
+            clk => clk,
+            reset => reset,
+            increment => buttons(3),
+            decrement => buttons(4),
+            enable => B_var,
+            color_value => B_value
+        );
+
+
+end Behavioral;
